7bf02559e38d925644cefae3e8f924a0fcd0d8ad
hytham defrawy <hythamdefrawy@gmail.com>
2021-12-02 13:34:17 -0500

    Added user route

--- server.js
+++ server.js
@@ -35,13 +35,15 @@ app.use(express.static("public"));
 
 // Separated Routes for each Resource
 // Note: Feel free to replace the example routes below with your own
+const homeRoutes = require("./routes/index");
 const usersRoutes = require("./routes/users");
 const widgetsRoutes = require("./routes/widgets");
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
-app.use("/api/users", usersRoutes(db));
-app.use("/api/widgets", widgetsRoutes(db));
+app.use("/home", homeRoutes(db));
+app.use("/users", usersRoutes(db));
+app.use("/widgets", widgetsRoutes(db));
 // Note: mount other resources here, using the same pattern above
 
 // Home page
