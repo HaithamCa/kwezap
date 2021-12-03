4b25c90fa74ff11ec07f55318003101489fecf61
hytham defrawy <hythamdefrawy@gmail.com>
2021-12-03 08:21:27 -0500

    Create a post route to createquiz

--- routes/users.js
+++ routes/users.js
@@ -25,14 +25,14 @@ module.exports = (db) => {
   // });
 
   // The user homepage when they can see their quizzes
-  router.get("/:id", (req, res) => {
+  router.get("/:user_id", (req, res) => {
     db.query(`SELECT users.id AS user_id, users.username, quizzes.id AS quiz_id, quizzes.title, quizzes.description, quizzes.public
               from users JOIN quizzes on owner_id = users.id
               WHERE owner_id = $1
               GROUP BY users.id, users.username, quizzes.id, quizzes.title, quizzes.description, quizzes.public;`, 
-              [req.params.id])
+              [req.params.user_id])
       .then(user => {
-        const templateVar = {userInfo: user.rows, user_id: req.params.id};
+        const templateVar = {userInfo: user.rows, user_id: req.params.user_id};
         res.render("../views/users", templateVar);
       })
       .catch(err => {
@@ -46,13 +46,26 @@ module.exports = (db) => {
   router.get("/createquiz/:user_id", (req, res) => {
     let templateVar = { user_id: req.params.user_id };
     res.render('../views/create_quiz', templateVar);
-  })
+  });
+
+  // storing created quizzes into the database
+  router.post("/createquiz/:user_id", (req, res) => {
+    let queryString = `INSERT INTO quizzes (owner_id, title, description, public)
+                VALUES ($1, $2, $3, $4) RETURNING id;`;
+    let values = [req.params.user_id, req.body.title, req.body.description, req.body.public];
+    db.query(queryString, values)
+      .then(data => {
+        let quizId = data.rows[0].id
+        res.redirect(`/quiz/${quizId}/questions`);
+      })
+      .catch(err => {
+        const errMsg = 'Please complete the form.'
+        res.send(errMsg);
+      });
+  });
 
   
 
 
   return router;
 };
-
-
-
